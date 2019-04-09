8252023b11841ff62dd4289bd941c716770bafa2
DEMI0815 <807205251@qq.com>
2019-04-09 21:09:35 +0800

    update the practice07

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+import practice06.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduceWith(Student student) {
+        if (klass == student.getKlass()) {
+            return super.introduce() + " I am a Teacher. I teach "+ student.getName() +".";
+        }else {
+            return super.introduce() + " I am a Teacher. I don't teach "+ student.getName() +".";
+        }
+    }
+
+    public String introduce() {
+        if (klass != null)
+            return super.introduce() + " I am a Teacher. I teach Class "+ klass.getNumber() +".";
+        else
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
